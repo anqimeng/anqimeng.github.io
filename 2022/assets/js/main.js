fe92b0d1f4d8fdeb6b8e288dad8f3063bf98f3fe
anqimeng <angieee627@gmail.com>
2020-06-19 01:55:23 +1000

    upload

--- 2022/assets/js/main.js
+++ 2022/assets/js/main.js
@@ -94,4 +94,37 @@ $('.pic').click(function() {
   $(this).removeClass('show');
 });
 
+
+
+// time out
+setTimeout(function(){
+    $('.title-1').addClass('active')
+},1000);
+
+setTimeout(function(){
+    $('.title-2').addClass('active')
+},2000);
+
+setTimeout(function(){
+    $('.title-3').addClass('active')
+},3000);
+
+setTimeout(function(){
+    $('.image-container').addClass('active')
+},4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
