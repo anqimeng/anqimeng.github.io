6147d48612b827e5df9f3dff08e3cab231f0234f
anqimeng <angieee627@gmail.com>
2020-11-22 21:28:46 +1100

    nab

--- notabook/assets/js/main.js
+++ notabook/assets/js/main.js
@@ -5,6 +5,13 @@ $( ".keyword" ).click(function() {
   $( ".k-toggle" ).slideToggle( "slow" );
 });
 
+$( ".foreword" ).click(function() {
+  $( ".f-toggle" ).slideToggle( "slow" );
+});
+
+$( ".no" ).click(function() {
+  $( ".t-toggle" ).slideToggle( "slow" );
+});
 
 
 
